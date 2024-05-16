4415fc3792dd72ccc626b65aa6691105b7bbb43d
Clay Dowling <clay@lazarusid.com>
2024-05-16 18:24:25 -0400

    Added card text generation

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.22.0
 
 require (
 	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/mattn/go-runewidth v0.0.12 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
 	golang.org/x/text v0.15.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	svg "github.com/ajstarks/svgo"
+	"github.com/muesli/reflow/wordwrap"
 	"github.com/skip2/go-qrcode"
 	"golang.org/x/text/cases"
 	"golang.org/x/text/language"
@@ -16,8 +18,10 @@ import (
 func main() {
 	var srcfile string
 	var baseurl string
+	var baseText string
 	flag.StringVar(&srcfile, "source", "", "File to read URL suffixes from")
 	flag.StringVar(&baseurl, "base", "https://example.com", "Base url for generated QR Codes")
+	flag.StringVar(&baseText, "text", "", "Text to include on card with title")
 	flag.Parse()
 
 	if srcfile == "" {
@@ -48,7 +52,8 @@ func main() {
 
 	csr := cases.Title(language.AmericanEnglish)
 	for _, name := range fileLines {
-		title := csr.String(name)
+		titleText := strings.Replace(name, "-", " ", -1)
+		title := csr.String(titleText)
 		imagefile := fmt.Sprintf("%s.png", name)
 		outfile := fmt.Sprintf("%s.svg", name)
 
@@ -58,10 +63,19 @@ func main() {
 		}
 		canvas := svg.New(outwriter)
 
-		canvas.Start(256, 300)
+		cardText := title
+		if baseText != "" {
+			cardText += " "
+			cardText += baseText
+		}
+
+		cardLines := strings.Split(wordwrap.String(cardText, 14), "\n")
+
+		canvas.Startunit(3, 2, "in", "style=\"font-family: Arial\"")
 		canvas.Title(fmt.Sprintf("QR Code for %s", title))
-		canvas.Image(0, 0, 256, 256, imagefile)
-		canvas.Text(128, 257, title, "text-anchor: middle; color: black; font-family: Arial; font-size: 10pt")
+		canvas.Image(144, 0, 144, 144, imagefile)
+		canvas.Textlines(4, 28, cardLines, 20, 24, "black", "start")
+		canvas.Text(216, 160, name, "text-anchor: middle; color: black; font-family: Arial; font-size: 10pt")
 		canvas.End()
 	}
 
